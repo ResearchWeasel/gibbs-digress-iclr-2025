0d723af038d039081428ce4e4af37f0ba299dcd0
ResearchWeasel <research_rabbit@protonmail.com>
2024-09-30 16:27:46 +0200

    Add DiGress base

---
+++ README.md
@@ -0,0 +1,126 @@
+# DiGress: Discrete Denoising diffusion models for graph generation
+
+Update (Nov 20th, 2023): Working with large graphs (more than 100-200 nodes)? Consider using SparseDiff, a sparse version of DiGress: https://github.com/qym7/SparseDiff
+
+Update (July 11th, 2023): the code now supports multi-gpu. Please update all libraries according to the instructions. 
+All datasets should now download automatically
+
+  - For the conditional generation experiments, check the `guidance` branch.
+  - If you are training new models from scratch, we recommand to use the `fixed_bug` branch in which some neural
+network layers have been fixed. The `fixed_bug` branch has not been evaluated, but should normally perform better.
+If you train the `fixed_bug` branch on datasets provided in this code, we would be happy to know the results.
+
+## Environment installation
+This code was tested with PyTorch 2.0.1, cuda 11.8 and torch_geometrics 2.3.1
+
+  - Download anaconda/miniconda if needed
+  - Create a rdkit environment that directly contains rdkit:
+    
+    ```conda create -c conda-forge -n digress rdkit=2023.03.2 python=3.9```
+  - `conda activate digress`
+  - Check that this line does not return an error:
+    
+    ``` python3 -c 'from rdkit import Chem' ```
+  - Install graph-tool (https://graph-tool.skewed.de/): 
+    
+    ```conda install -c conda-forge graph-tool=2.45```
+  - Check that this line does not return an error:
+    
+    ```python3 -c 'import graph_tool as gt' ```
+  - Install the nvcc drivers for your cuda version. For example:
+    
+    ```conda install -c "nvidia/label/cuda-11.8.0" cuda```
+  - Install a corresponding version of pytorch, for example: 
+    
+    ```pip3 install torch==2.0.1 --index-url https://download.pytorch.org/whl/cu118```
+  - Install other packages using the requirement file: 
+    
+    ```pip install -r requirements.txt```
+
+  - Run:
+    
+    ```pip install -e .```
+
+  - Navigate to the ./src/analysis/orca directory and compile orca.cpp: 
+    
+     ```g++ -O2 -std=c++11 -o orca orca.cpp```
+
+Note: graph_tool and torch_geometric currently seem to conflict on MacOS, I have not solved this issue yet.
+
+## Run the code
+  
+  - All code is currently launched through `python3 main.py`. Check hydra documentation (https://hydra.cc/) for overriding default parameters.
+  - To run the debugging code: `python3 main.py +experiment=debug.yaml`. We advise to try to run the debug mode first
+    before launching full experiments.
+  - To run a code on only a few batches: `python3 main.py general.name=test`.
+  - To run the continuous model: `python3 main.py model=continuous`
+  - To run the discrete model: `python3 main.py`
+  - You can specify the dataset with `python3 main.py dataset=guacamol`. Look at `configs/dataset` for the list
+of datasets that are currently available
+    
+## Checkpoints
+
+**My drive account has unfortunately been deleted, and I have lost access to the checkpoints. If you happen to have a downloaded checkpoint stored locally, I would be glad if you could send me an email at vignac.clement@gmail.com or raise a Github issue.**
+
+The following checkpoints should work with the latest commit:
+
+  - QM9 (heavy atoms only): https://drive.switch.ch/index.php/s/8IhyGE4giIW1wV3/download?path=%2F&files=planar.ckpt \\
+  
+  - Planar: https://drive.switch.ch/index.php/s/8IhyGE4giIW1wV3/download?path=%2F&files=planar.ckpt \\
+
+  - MOSES (the model in the paper was trained a bit longer than this one): ~~https://drive.switch.ch/index.php/s/DBbvfMmezjg6KUm~~ \\
+    Performance of this checkpoint:
+    - Test NLL: 203.8171 
+    - `{'valid': 0.86032, 'unique@1000': 1.0, 'unique@10000': 0.9999, 'FCD/Test': 0.6176261401223826, 'SNN/Test': 0.5493580505032953, 'Frag/Test': 0.9986637035374839, 'Scaf/Test': 0.8997144919185305, 'FCD/TestSF': 1.2799741890619032, 'SNN/TestSF': 0.5231424506655995, 'Frag/TestSF': 0.9968362360368359, 'Scaf/TestSF': 0.11830576038721641, 'IntDiv': 0.8550915438149056, 'IntDiv2': 0.8489191659624407, 'Filters': 0.9707550678817184, 'logP': 0.02719348046624242, 'SA': 0.05725088257521343, 'QED': 0.0043940205061221965, 'weight': 0.7913020095007184, 'Novelty': 0.9442790697674419}`
+
+  - SBM: ~~https://drive.switch.ch/index.php/s/rxWFVQX4Cu4Vq5j~~ \\
+    Performance of this checkpoint:
+    - Test NLL: 4757.903
+    - `{'spectre': 0.0060240439382095445, 'clustering': 0.05020166160905111, 'orbit': 0.04615866844490847, 'sbm_acc': 0.675, 'sampling/frac_unique': 1.0, 'sampling/frac_unique_non_iso': 1.0, 'sampling/frac_unic_non_iso_valid': 0.625, 'sampling/frac_non_iso': 1.0}`
+
+
+The following checkpoints require to revert to commit `682e59019dd33073b1f0f4d3aaba7de6a308602e` and run `pip install -e .`:
+
+  - Guacamol: ~~https://drive.switch.ch/index.php/s/jjM3pIHdxWrUGOH~~
+
+## Generated samples
+
+We provide the generated samples for some of the models. If you have retrained a model from scratch for which the samples are
+not available yet, we would be very happy if you could send them to us!
+
+
+## Troubleshooting 
+
+`PermissionError: [Errno 13] Permission denied: '/home/vignac/DiGress/src/analysis/orca/orca'`: You probably did not compile orca.
+    
+
+## Use DiGress on a new dataset
+
+To implement a new dataset, you will need to create a new file in the `src/datasets` folder. Depending on whether you are considering
+molecules or abstract graphs, you can base this file on `moses_dataset.py` or `spectre_datasets.py`, for example. 
+This file should implement a `Dataset` class to process the data (check [PyG documentation](https://pytorch-geometric.readthedocs.io/en/latest/tutorial/create_dataset.html)), 
+as well as a `DatasetInfos` class that is used to define the noise model and some metrics.
+
+For molecular datasets, you'll need to specify several things in the DatasetInfos:
+  - The atom_encoder, which defines the one-hot encoding of the atom types in your dataset
+  - The atom_decoder, which is simply the inverse mapping of the atom encoder
+  - The atomic weight for each atom atype
+  - The most common valency for each atom type
+
+The node counts and the distribution of node types and edge types can be computed automatically using functions from `AbstractDataModule`.
+
+Once the dataset file is written, the code in main.py can be adapted to handle the new dataset, and a new file can be added in `configs/dataset`.
+
+
+## Cite the paper
+
+```
+@inproceedings{
+vignac2023digress,
+title={DiGress: Discrete Denoising diffusion for graph generation},
+author={Clement Vignac and Igor Krawczuk and Antoine Siraudin and Bohan Wang and Volkan Cevher and Pascal Frossard},
+booktitle={The Eleventh International Conference on Learning Representations },
+year={2023},
+url={https://openreview.net/forum?id=UaAD-Nu86WX}
+}
+```
---
+++ src/main.py
@@ -0,0 +1,223 @@
+import graph_tool as gt
+import os
+import pathlib
+import warnings
+
+import torch
+torch.cuda.empty_cache()
+import hydra
+from omegaconf import DictConfig
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.utilities.warnings import PossibleUserWarning
+
+from src import utils
+from metrics.abstract_metrics import TrainAbstractMetricsDiscrete, TrainAbstractMetrics
+
+from diffusion_model import LiftedDenoisingDiffusion
+from diffusion_model_discrete import DiscreteDenoisingDiffusion
+from diffusion.extra_features import DummyExtraFeatures, ExtraFeatures
+
+
+warnings.filterwarnings("ignore", category=PossibleUserWarning)
+
+
+def get_resume(cfg, model_kwargs):
+    """ Resumes a run. It loads previous config without allowing to update keys (used for testing). """
+    saved_cfg = cfg.copy()
+    name = cfg.general.name + '_resume'
+    resume = cfg.general.test_only
+    if cfg.model.type == 'discrete':
+        model = DiscreteDenoisingDiffusion.load_from_checkpoint(resume, **model_kwargs)
+    else:
+        model = LiftedDenoisingDiffusion.load_from_checkpoint(resume, **model_kwargs)
+    cfg = model.cfg
+    cfg.general.test_only = resume
+    cfg.general.name = name
+    cfg = utils.update_config_with_new_keys(cfg, saved_cfg)
+    return cfg, model
+
+
+def get_resume_adaptive(cfg, model_kwargs):
+    """ Resumes a run. It loads previous config but allows to make some changes (used for resuming training)."""
+    saved_cfg = cfg.copy()
+    # Fetch path to this file to get base path
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    root_dir = current_path.split('outputs')[0]
+
+    resume_path = os.path.join(root_dir, cfg.general.resume)
+
+    if cfg.model.type == 'discrete':
+        model = DiscreteDenoisingDiffusion.load_from_checkpoint(resume_path, **model_kwargs)
+    else:
+        model = LiftedDenoisingDiffusion.load_from_checkpoint(resume_path, **model_kwargs)
+    new_cfg = model.cfg
+
+    for category in cfg:
+        for arg in cfg[category]:
+            new_cfg[category][arg] = cfg[category][arg]
+
+    new_cfg.general.resume = resume_path
+    new_cfg.general.name = new_cfg.general.name + '_resume'
+
+    new_cfg = utils.update_config_with_new_keys(new_cfg, saved_cfg)
+    return new_cfg, model
+
+
+
+@hydra.main(version_base='1.3', config_path='../configs', config_name='config')
+def main(cfg: DictConfig):
+    dataset_config = cfg["dataset"]
+
+    if dataset_config["name"] in ['sbm', 'comm20', 'planar']:
+        from datasets.spectre_dataset import SpectreGraphDataModule, SpectreDatasetInfos
+        from analysis.spectre_utils import PlanarSamplingMetrics, SBMSamplingMetrics, Comm20SamplingMetrics
+        from analysis.visualization import NonMolecularVisualization
+
+        datamodule = SpectreGraphDataModule(cfg)
+        if dataset_config['name'] == 'sbm':
+            sampling_metrics = SBMSamplingMetrics(datamodule)
+        elif dataset_config['name'] == 'comm20':
+            sampling_metrics = Comm20SamplingMetrics(datamodule)
+        else:
+            sampling_metrics = PlanarSamplingMetrics(datamodule)
+
+        dataset_infos = SpectreDatasetInfos(datamodule, dataset_config)
+        train_metrics = TrainAbstractMetricsDiscrete() if cfg.model.type == 'discrete' else TrainAbstractMetrics()
+        visualization_tools = NonMolecularVisualization()
+
+        if cfg.model.type == 'discrete' and cfg.model.extra_features is not None:
+            extra_features = ExtraFeatures(cfg.model.extra_features, dataset_info=dataset_infos)
+        else:
+            extra_features = DummyExtraFeatures()
+        domain_features = DummyExtraFeatures()
+
+        dataset_infos.compute_input_output_dims(datamodule=datamodule, extra_features=extra_features,
+                                                domain_features=domain_features)
+
+        model_kwargs = {'dataset_infos': dataset_infos, 'train_metrics': train_metrics,
+                        'sampling_metrics': sampling_metrics, 'visualization_tools': visualization_tools,
+                        'extra_features': extra_features, 'domain_features': domain_features}
+
+    elif dataset_config["name"] in ['qm9', 'guacamol', 'moses']:
+        from metrics.molecular_metrics import TrainMolecularMetrics, SamplingMolecularMetrics
+        from metrics.molecular_metrics_discrete import TrainMolecularMetricsDiscrete
+        from diffusion.extra_features_molecular import ExtraMolecularFeatures
+        from analysis.visualization import MolecularVisualization
+
+        if dataset_config["name"] == 'qm9':
+            from datasets import qm9_dataset
+            datamodule = qm9_dataset.QM9DataModule(cfg)
+            dataset_infos = qm9_dataset.QM9infos(datamodule=datamodule, cfg=cfg)
+            train_smiles = qm9_dataset.get_train_smiles(cfg=cfg, train_dataloader=datamodule.train_dataloader(),
+                                                        dataset_infos=dataset_infos, evaluate_dataset=False)
+        elif dataset_config['name'] == 'guacamol':
+            from datasets import guacamol_dataset
+            datamodule = guacamol_dataset.GuacamolDataModule(cfg)
+            dataset_infos = guacamol_dataset.Guacamolinfos(datamodule, cfg)
+            train_smiles = None
+
+        elif dataset_config.name == 'moses':
+            from datasets import moses_dataset
+            datamodule = moses_dataset.MosesDataModule(cfg)
+            dataset_infos = moses_dataset.MOSESinfos(datamodule, cfg)
+            train_smiles = None
+        else:
+            raise ValueError("Dataset not implemented")
+
+        if cfg.model.type == 'discrete' and cfg.model.extra_features is not None:
+            extra_features = ExtraFeatures(cfg.model.extra_features, dataset_info=dataset_infos)
+            domain_features = ExtraMolecularFeatures(dataset_infos=dataset_infos)
+        else:
+            extra_features = DummyExtraFeatures()
+            domain_features = DummyExtraFeatures()
+
+        dataset_infos.compute_input_output_dims(datamodule=datamodule, extra_features=extra_features,
+                                                domain_features=domain_features)
+
+        if cfg.model.type == 'discrete':
+            train_metrics = TrainMolecularMetricsDiscrete(dataset_infos)
+        else:
+            train_metrics = TrainMolecularMetrics(dataset_infos)
+
+        # We do not evaluate novelty during training
+        sampling_metrics = SamplingMolecularMetrics(dataset_infos, train_smiles)
+        visualization_tools = MolecularVisualization(cfg.dataset.remove_h, dataset_infos=dataset_infos)
+
+        model_kwargs = {'dataset_infos': dataset_infos, 'train_metrics': train_metrics,
+                        'sampling_metrics': sampling_metrics, 'visualization_tools': visualization_tools,
+                        'extra_features': extra_features, 'domain_features': domain_features}
+    else:
+        raise NotImplementedError("Unknown dataset {}".format(cfg["dataset"]))
+
+    if cfg.general.test_only:
+        # When testing, previous configuration is fully loaded
+        cfg, _ = get_resume(cfg, model_kwargs)
+        os.chdir(cfg.general.test_only.split('checkpoints')[0])
+    elif cfg.general.resume is not None:
+        # When resuming, we can override some parts of previous configuration
+        cfg, _ = get_resume_adaptive(cfg, model_kwargs)
+        os.chdir(cfg.general.resume.split('checkpoints')[0])
+
+    utils.create_folders(cfg)
+
+    if cfg.model.type == 'discrete':
+        model = DiscreteDenoisingDiffusion(cfg=cfg, **model_kwargs)
+    else:
+        model = LiftedDenoisingDiffusion(cfg=cfg, **model_kwargs)
+
+    callbacks = []
+    if cfg.train.save_model:
+        checkpoint_callback = ModelCheckpoint(dirpath=f"checkpoints/{cfg.general.name}",
+                                              filename='{epoch}',
+                                              monitor='val/epoch_NLL',
+                                              save_top_k=5,
+                                              mode='min',
+                                              every_n_epochs=1)
+        last_ckpt_save = ModelCheckpoint(dirpath=f"checkpoints/{cfg.general.name}", filename='last', every_n_epochs=1)
+        callbacks.append(last_ckpt_save)
+        callbacks.append(checkpoint_callback)
+
+    if cfg.train.ema_decay > 0:
+        ema_callback = utils.EMA(decay=cfg.train.ema_decay)
+        callbacks.append(ema_callback)
+
+    name = cfg.general.name
+    if name == 'debug':
+        print("[WARNING]: Run is called 'debug' -- it will run with fast_dev_run. ")
+
+    use_gpu = cfg.general.gpus > 0 and torch.cuda.is_available()
+    trainer = Trainer(gradient_clip_val=cfg.train.clip_grad,
+                      strategy="ddp_find_unused_parameters_true",  # Needed to load old checkpoints
+                      accelerator='gpu' if use_gpu else 'cpu',
+                      devices=cfg.general.gpus if use_gpu else 1,
+                      max_epochs=cfg.train.n_epochs,
+                      check_val_every_n_epoch=cfg.general.check_val_every_n_epochs,
+                      fast_dev_run=cfg.general.name == 'debug',
+                      enable_progress_bar=False,
+                      callbacks=callbacks,
+                      log_every_n_steps=50 if name != 'debug' else 1,
+                      logger = [])
+
+    if not cfg.general.test_only:
+        trainer.fit(model, datamodule=datamodule, ckpt_path=cfg.general.resume)
+        if cfg.general.name not in ['debug', 'test']:
+            trainer.test(model, datamodule=datamodule)
+    else:
+        # Start by evaluating test_only_path
+        trainer.test(model, datamodule=datamodule, ckpt_path=cfg.general.test_only)
+        if cfg.general.evaluate_all_checkpoints:
+            directory = pathlib.Path(cfg.general.test_only).parents[0]
+            print("Directory:", directory)
+            files_list = os.listdir(directory)
+            for file in files_list:
+                if '.ckpt' in file:
+                    ckpt_path = os.path.join(directory, file)
+                    if ckpt_path == cfg.general.test_only:
+                        continue
+                    print("Loading checkpoint", ckpt_path)
+                    trainer.test(model, datamodule=datamodule, ckpt_path=ckpt_path)
+
+
+if __name__ == '__main__':
+    main()
